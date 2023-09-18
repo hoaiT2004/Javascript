10f34370ea5ff5775c2f7fdf516b29e73b3281d9
Nguyễn Xuân Hòa <nguyenxuanhoa8b@gmail.com>
2023-09-18 07:58:06 +0700

    l1

---
+++ video.js
@@ -0,0 +1,12 @@
+
+let x = "5"
+let y = Number(x)
+let y2 = +x;
+// let z = 6
+// let tex = `hello
+
+// \<br>cac`;
+// let tex2 = "hello/kkkk";
+console.log(`${+x+y}`)
+// let text = " 12 3 "
+// document.getElementById("demo").innerHTML=  (x==y)+ "<br>" +text.trim().length;
